26c2437696593c0473218d9780426776cc562623
Yan P Aung <yan.pye.aung@gmail.com>
2018-03-10 20:24:50 -0800

    Keyboard event

--- keyboard.py
+++ keyboard.py
@@ -17,6 +17,7 @@ class NamePad(object):
 
         self.img = PhotoImage(file="./imgs/anna.gif")
         self.text = Text(master)
+        self.text.bind("<Key>", self.key)
         self.text.pack(padx=20, pady=20)
 
         self.greet_button = Button(master, text="Show Image", command=self.show)
@@ -29,6 +30,8 @@ class NamePad(object):
         self.text.image_create(END, image=self.img) # Example 1
         self.text.window_create(END, window=Label(self.text, image=self.img))
 
+    def key(self, event):
+        print ("pressed", repr(event.char))
 
 if __name__ == '__main__':
     root = Tk()
