0505201127cfd9928d995558e75a468fbd224dfb
Yan P Aung <yan.pye.aung@gmail.com>
2018-03-10 19:48:26 -0800

    Show image

--- keyboard.py
+++ keyboard.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label, Button
+from tkinter import Tk, Label, Button, Text, PhotoImage, END
 
 class NamePad(object):
     def __init__(self, master):
@@ -8,14 +8,19 @@ class NamePad(object):
         self.label = Label(master, text="Type in your name and see what it looks like!")
         self.label.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.img = PhotoImage(file="./imgs/anna.gif")
+        self.text = Text(master)
+        self.text.pack(padx=20, pady=20)
+
+        self.greet_button = Button(master, text="Show Image", command=self.show)
         self.greet_button.pack()
 
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
 
-    def greet(self):
-        print("Greetings!")
+    def show(self):
+        self.text.image_create(END, image=self.img) # Example 1
+        self.text.window_create(END, window=Label(self.text, image=self.img))
 
 
 if __name__ == '__main__':
