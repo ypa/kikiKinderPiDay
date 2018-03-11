6215714afa912696fee4bafa467f5319d11522b1
Yan P Aung <yan.pye.aung@gmail.com>
2018-03-10 16:32:00 -0800

    Refactor

--- namepad.py
+++ namepad.py
@@ -26,12 +26,11 @@ class Namepad:
         #set icon
         try:
             self.__root.wm_iconbitmap("Namepad.ico") #GOT TO FIX THIS ERROR (ICON)
-        except:
-            pass
-
+        except Exception as e:
+            print("Icon missing")
 
         #set the window text
-        self.__root.title("Untitled - Notepad")
+        self.__root.title("New - Namepad")
 
         #center the window
         screenWidth = self.__root.winfo_screenwidth()
@@ -50,28 +49,27 @@ class Namepad:
 
         self.__thisTextArea.grid(sticky=N+E+S+W)
 
-        self.__thisFileMenu.add_command(label="New",command=self.__newFile)
-        self.__thisFileMenu.add_command(label="Open",command=self.__openFile)
-        self.__thisFileMenu.add_command(label="Save",command=self.__saveFile)
+        self.__thisFileMenu.add_command(label="New", command=self.__newFile)
+        self.__thisFileMenu.add_command(label="Open", command=self.__openFile)
+        self.__thisFileMenu.add_command(label="Save", command=self.__saveFile)
         self.__thisFileMenu.add_separator()
-        self.__thisFileMenu.add_command(label="Exit",command=self.__quitApplication)
-        self.__thisMenuBar.add_cascade(label="File",menu=self.__thisFileMenu)
+        self.__thisFileMenu.add_command(label="Exit", command=self.__quitApplication)
+        self.__thisMenuBar.add_cascade(label="File", menu=self.__thisFileMenu)
 
-        self.__thisEditMenu.add_command(label="Cut",command=self.__cut)
-        self.__thisEditMenu.add_command(label="Copy",command=self.__copy)
-        self.__thisEditMenu.add_command(label="Paste",command=self.__paste)
-        self.__thisMenuBar.add_cascade(label="Edit",menu=self.__thisEditMenu)
+        self.__thisEditMenu.add_command(label="Cut", command=self.__cut)
+        self.__thisEditMenu.add_command(label="Copy", command=self.__copy)
+        self.__thisEditMenu.add_command(label="Paste", command=self.__paste)
+        self.__thisMenuBar.add_cascade(label="Edit", menu=self.__thisEditMenu)
 
-        self.__thisHelpMenu.add_command(label="About Notepad",command=self.__showAbout)
-        self.__thisMenuBar.add_cascade(label="Help",menu=self.__thisHelpMenu)
+        self.__thisHelpMenu.add_command(label="About Notepad", command=self.__showAbout)
+        self.__thisMenuBar.add_cascade(label="Help", menu=self.__thisHelpMenu)
 
         self.__root.config(menu=self.__thisMenuBar)
 
         self.__thisScrollBar.pack(side=RIGHT,fill=Y)
         self.__thisScrollBar.config(command=self.__thisTextArea.yview)
         self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
-    
-        
+
     def __quitApplication(self):
         self.__root.destroy()
         #exit()
@@ -80,7 +78,7 @@ class Namepad:
         showinfo("Notepad","Created by: Ferdinand Silva (http://ferdinandsilva.com)")
 
     def __openFile(self):
-        
+
         self.__file = askopenfilename(defaultextension=".txt",filetypes=[("All Files","*.*"),("Text Documents","*.txt")])
 
         if self.__file == "":
@@ -90,7 +88,7 @@ class Namepad:
             #try to open the file
             #set the window title
             self.__root.title(os.path.basename(self.__file) + " - Notepad")
-            self.__thisTextArea.delete(1.0,END)
+            self.__thisTextArea.delete(1.0, END)
 
             file = open(self.__file,"r")
 
@@ -98,11 +96,10 @@ class Namepad:
 
             file.close()
 
-        
     def __newFile(self):
         self.__root.title("Untitled - Notepad")
         self.__file = None
-        self.__thisTextArea.delete(1.0,END)
+        self.__thisTextArea.delete(1.0, END)
 
     def __saveFile(self):
 
@@ -115,13 +112,13 @@ class Namepad:
             else:
                 #try to save the file
                 file = open(self.__file,"w")
-                file.write(self.__thisTextArea.get(1.0,END))
+                file.write(self.__thisTextArea.get(1.0, END))
                 file.close()
                 #change the window title
                 self.__root.title(os.path.basename(self.__file) + " - Notepad")
         else:
             file = open(self.__file,"w")
-            file.write(self.__thisTextArea.get(1.0,END))
+            file.write(self.__thisTextArea.get(1.0, END))
             file.close()
 
     def __cut(self):
